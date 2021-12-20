e66489c3e290ddff5970a60a2afaa9d97e9347ef
Daniele <dsdfasdfa.gmail.com>
2021-12-20 08:58:52 +0100

    Sumar numeros

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
-    #include <iostream> 
-    #include <string> 
-    using namespace std; 
-     
-    int main() 
-    {
-       cout << "hola mundo" << endl;
-       
-     
-      return 0; 
-    }
+#include <iostream> 
+#include <string> 
+using namespace std;
+
+void sumar(int &num1, int &num2){
+
+  int total;
+
+  total = num1 + num2;
+
+  cout << total;
+
+}
+
+int main() {
+
+  int numero1, numero2;
+
+  cout << "Buenas eliga los numeros para sumar" << endl;
+  cout << "Eliga el numero 1" << endl;
+  
+  cin >> numero1;
+
+  cout << "Eliga el numero 2" << endl;
+
+  cin >> numero2;
+
+  cout << "El total es ";
+  
+  sumar(numero1,numero2);
+
+  cout << endl;
+
+
+return 0; 
+}
